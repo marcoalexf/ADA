25bd6399086049d887752c680b6c89ed63dee25f
marcoalexf <marcoasfran@gmail.com>
2018-05-25 21:10:48 +0100

    ciclo infinitp

--- ADA/ADA_T3/src/Main.java
+++ ADA/ADA_T3/src/Main.java
@@ -14,20 +14,20 @@ public class Main {
 		int w = Integer.parseInt(dimensions[0]);
 		int h = Integer.parseInt(dimensions[1]);
 
-		String [][] map = new String [w][h];
+		String [][] map = new String [h][w];
 		
 		for(int i = 0; i < h; i++) {
 			String params [] = input.readLine().split(" ");
-			for(int k = 0; k < params.length; k++) {
+			for(int k = 0; k < w; k++) {
 				map[i][k] = params[k]; // reverse it so the matrix does x THEN y
 			}
 		}
+		
 		Problem p = new Problem(map, w ,h);
 		
 		try {
 			System.out.println(p.solve().getD());
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
